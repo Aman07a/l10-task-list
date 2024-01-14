1c969cb4f5c225500dd11f13c608133499ffb66d
Aman07a <aman16.aa17@gmail.com>
2024-01-14 12:13:31 +0100

    Sessions, Errors & Flash Messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,14 +2,17 @@
 <html>
 
 <head>
-  <title>Laravel 10 Task List App</title>
+    <title>Laravel 10 Task List App</title>
 </head>
 
 <body>
-  <h1>@yield('title')</h1>
-  <div>
-    @yield('content')
-  </div>
+    <h1>@yield('title')</h1>
+    <div>
+        @if (session()->has('success'))
+            <div>{{ session('success') }}</div>
+        @endif
+        @yield('content')
+    </div>
 </body>
 
 </html>
